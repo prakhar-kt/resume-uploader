93a9693d8db78041a00ee106d91e29ece5a150b0
prakhar-kt <rahkarp@gmail.com>
2025-08-26 13:20:14 +0530

    project setup

---
+++ app/resume/utils.py
@@ -0,0 +1,21 @@
+import os
+from uuid import uuid4
+from fastapi import UploadFile
+
+UPLOAD_DIR = "uploads"
+
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+
+async def save_uploaded_file(upload_file: UploadFile, sub_dir: str) -> str:
+    original_name: str = upload_file.filename or ""
+    ext = os.path.splitext(original_name)[-1]
+    filename = f"{uuid4().hex}{ext}"
+    dir_path = os.path.join(UPLOAD_DIR, sub_dir)
+    os.makedirs(dir_path, exist_ok=True)
+    file_path = os.path.join(dir_path, filename)
+    with open(file_path, "wb") as f:
+        content = await upload_file.read()
+        f.write(content)
+
+    return file_path
