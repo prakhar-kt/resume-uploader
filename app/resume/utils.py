127a4dfc093fd078e4c1ed0e91e98c5090e4dfdf
prakhar-kt <rahkarp@gmail.com>
2025-08-27 12:09:00 +0530

    add routes

--- app/resume/utils.py
+++ app/resume/utils.py
@@ -1,6 +1,6 @@
 import os
 from uuid import uuid4
-from fastapi import UploadFile
+from fastapi import HTTPException, UploadFile
 
 UPLOAD_DIR = "uploads"
 
@@ -9,6 +9,8 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 async def save_uploaded_file(upload_file: UploadFile, sub_dir: str) -> str:
     original_name: str = upload_file.filename or ""
+    if original_name == "":
+        raise HTTPException(status_code=400, detail="No File to upload")
     ext = os.path.splitext(original_name)[-1]
     filename = f"{uuid4().hex}{ext}"
     dir_path = os.path.join(UPLOAD_DIR, sub_dir)
